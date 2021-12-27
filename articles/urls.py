9120845d0ed9d16eae7b0f65dbe83e103de309d1
sh1ny1337 <48220442+sh1ny1337@users.noreply.github.com>
2021-12-27 20:33:31 +0200

    finished

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='detail'),
+
 ]
